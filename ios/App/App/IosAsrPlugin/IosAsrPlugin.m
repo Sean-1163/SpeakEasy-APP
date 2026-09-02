6677ab9dc1e19d5160b7e91b09bd3a5d61415c6c
Sean-1163 <liuxw15@lenovo.com>
2026-09-02 17:25:55 +0800

    ci: remove broken partial ios platform

--- ios/App/App/IosAsrPlugin/IosAsrPlugin.m
+++
@@ -1,9 +0,0 @@
-// IosAsrPlugin.m — Capacitor plugin registration
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(IosAsrPlugin, "IosAsr",
-    CAP_PLUGIN_METHOD(requestPermissions, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(isAvailable, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(startListening, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(stopListening, CAPPluginReturnPromise);
-)
